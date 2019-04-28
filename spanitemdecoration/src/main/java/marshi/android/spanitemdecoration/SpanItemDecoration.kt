91095410701b9482e8830d67970073560d117ae5
mukai_masaki <mukai_masaki@cyberagent.co.jp>
2019-04-29 03:07:49 +0900

    abstract

--- spanitemdecoration/src/main/java/marshi/android/spanitemdecoration/SpanItemDecoration.kt
+++ spanitemdecoration/src/main/java/marshi/android/spanitemdecoration/SpanItemDecoration.kt
@@ -2,53 +2,39 @@ package marshi.android.spanitemdecoration
 
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
 import android.view.View
 import androidx.core.view.children
 import androidx.recyclerview.widget.RecyclerView
 
-abstract class SpanItemDecoration(
+abstract class SpanItemDecoration<T>(
     context: Context,
     open val groupAdapter: RecyclerView.Adapter<*>
 ) : RecyclerView.ItemDecoration() {
 
-    open val textPaddingTop = 0
-    open val textSize = 0
-    open val textPaddingBottom = 0
-    open val textLeftSpace = 0
-
-    open val paint = Paint().apply {
-        style = Paint.Style.FILL
-        textSize = 100f
-        color = Color.BLACK
-        isAntiAlias = true
-    }
-
     override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
-        var prevText: String? = null
+        var prevAsset: T? = null
         parent.children.forEachIndexed { index, view ->
             val position = parent.getChildAdapterPosition(view)
-            val text = text(position) ?: return@forEachIndexed
-            if (prevText == text) {
+            val currentAsset = text(position) ?: return@forEachIndexed
+            if (prevAsset == currentAsset) {
                 return@forEachIndexed
             }
-            prevText = text
-            val nextText = text(position + 1)
-            val drawParameter = drawParameter(position, view, prevText, text, nextText)
+            prevAsset = currentAsset
+            val nextAsset = text(position + 1)
+            val drawParameter = drawParameter(position, view, prevAsset, currentAsset, nextAsset)
             draw(c, drawParameter)
         }
     }
 
-    protected abstract fun text(position: Int): String?
+    protected abstract fun text(position: Int): T?
 
     protected abstract fun draw(canvas: Canvas, drawParameter: DrawParameter)
 
     protected abstract fun drawParameter(
         position: Int,
         view: View,
-        prevText: String?,
-        text: String,
-        nextText: String?
+        prevText: T?,
+        text: T,
+        nextText: T?
     ): DrawParameter
 }
--- spanitemdecoration/src/main/java/marshi/android/spanitemdecoration/TextSpanItemDecoration.kt
+++ spanitemdecoration/src/main/java/marshi/android/spanitemdecoration/TextSpanItemDecoration.kt
@@ -10,46 +10,28 @@ import androidx.recyclerview.widget.RecyclerView
 class TextSpanItemDecoration(
     context: Context,
     override val groupAdapter: RecyclerView.Adapter<*>
-) : SpanItemDecoration(
+) : SpanItemDecoration<String>(
     context, groupAdapter
 ) {
-
     private val resources = context.resources
-    override val textSize = resources.getDimensionPixelSize(
+    private val textSize = resources.getDimensionPixelSize(
         R.dimen.session_bottom_sheet_left_time_text_size
     )
-    override val textLeftSpace = resources.getDimensionPixelSize(
+    private val textLeftSpace = resources.getDimensionPixelSize(
         R.dimen.session_bottom_sheet_left_time_text_left
     )
-    override val textPaddingTop = resources.getDimensionPixelSize(
+    private val textPaddingTop = resources.getDimensionPixelSize(
         R.dimen.session_bottom_sheet_left_time_text_padding_top
     )
-    override val textPaddingBottom = resources.getDimensionPixelSize(
+    private val textPaddingBottom = resources.getDimensionPixelSize(
         R.dimen.session_bottom_sheet_left_time_text_padding_bottom
     )
-    override val paint = Paint().apply {
+    private val paint = Paint().apply {
         style = Paint.Style.FILL
         textSize = 100f
         color = Color.BLACK
         isAntiAlias = true
     }
-
-//    override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
-//        var prevText: String? = null
-//        parent.children.forEachIndexed { index, view ->
-//            val position = parent.getChildAdapterPosition(view)
-//            val text = getSessionTime(position) ?: return@forEachIndexed
-//            if (prevText == text) {
-//                return@forEachIndexed
-//            }
-//            prevText = text
-//            val nextText = getSessionTime(position + 1)
-//            var textBaselineY = view.top.coerceAtLeast(0) + textPaddingTop + textSize
-//            if (text != nextText) {
-//                textBaselineY = textBaselineY.coerceAtMost(view.bottom - textPaddingBottom)
-//            }
-//        }
-//    }
 
     override fun text(position: Int): String? {
         if (position < 0 || position >= groupAdapter.itemCount) {
